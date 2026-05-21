8897d5b677426826e77455be9f86d28e04421309
saptisunil1 <saptisunil1@gmail.com>
2026-05-21 19:48:08 +0100

    fragement change

--- src/component/startPage.js
+++ src/component/startPage.js
@@ -1,18 +1,17 @@
 import "../myCss.css";
-import { Container } from "react-bootstrap";
 import "../App.css";
 import StartPageCarousel from "./startPageCarousel";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Page1 from "./Page1";
 
 function Start() {
     const [showNext,setShowNext]= useState(false);
 
     return (
-        <Container fluid className="d-flex justify-content-center align-items-center min-vh-100 bg-desktop-black" >
+        <Fragment className="d-flex justify-content-center align-items-center min-vh-100 bg-desktop-black" >
             {!showNext && <StartPageCarousel onStart={() => setShowNext(true)} />}
             {showNext && <Page1></Page1>}
-        </Container>
+        </Fragment>
     );
 }
 
