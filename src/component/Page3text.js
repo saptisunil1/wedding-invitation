ccedddda73b397a969216022aa994bc5f12410c4
saptisunil1 <saptisunil1@gmail.com>
2026-05-25 12:50:19 +0100

    ui 3

--- src/component/Page3text.js
+++ src/component/Page3text.js
@@ -4,13 +4,13 @@ import "@fontsource/great-vibes";
 import { useRef } from "react";
 import { BsCalendarDateFill } from "react-icons/bs";
 import { FaLongArrowAltUp } from "react-icons/fa";
-import { IoLocation } from "react-icons/io5";
+import { IoLocation } from "react-icons/io5"
 
 function Page3text({ onSwipeDown, onSwipeUp }) {
     const startY = useRef(0);
     const currentY = useRef(0);
+    const isTouchingButton = useRef(false);
 
-    // detect swipe
     const handleTouchStart = (e) => {
         startY.current = e.touches[0].clientY;
     };
@@ -20,6 +20,8 @@ function Page3text({ onSwipeDown, onSwipeUp }) {
     };
 
     const handleTouchEnd = () => {
+        if (isTouchingButton.current) return;
+
         const diff = startY.current - currentY.current;
 
         if (Math.abs(diff) < 60) return;
@@ -32,58 +34,40 @@ function Page3text({ onSwipeDown, onSwipeUp }) {
     };
 
     return (
-        <div className="page3-wrapper">
+        <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
+            <div className="text-overlay1">
+                <p className="p9">Ceremony</p>
+                <p>
+                    <BsCalendarDateFill size={60} color="white" />
+                </p>
+                <p className="p12">November 19, 2026</p>
+                <p className="p11">11:30 am</p>
+                <p>
+                    <IoLocation size={60} color="white" />
+                </p>
+                <p className="p10">Kurumboor Mana</p>
+                <button
+                    className="location-btn"
+                    onTouchStart={() => (isTouchingButton.current = true)}
+                    onTouchEnd={() => (isTouchingButton.current = false)}
+                    onClick={(e) => {
+                        e.stopPropagation();
 
-            {/* SWIPE LAYER (ONLY BACKGROUND) */}
-            <div
-                className="swipe-layer"
-                onTouchStart={handleTouchStart}
-                onTouchMove={handleTouchMove}
-                onTouchEnd={handleTouchEnd}
-            />
-
-            {/* UI LAYER (BUTTONS + TEXT) */}
-            <div className="ui-layer">
-
-                <div className="text-overlay1">
-                    <p className="p9">Ceremony</p>
-
-                    <p>
-                        <BsCalendarDateFill size={50} color="white" />
-                    </p>
-
-                    <p className="p12">November 19, 2026</p>
-                    <p className="p11">11:30 AM</p>
-
-                    <p>
-                        <IoLocation size={50} color="white" />
-                    </p>
-
-                    <p className="p10">Kurumboor Mana</p>
-
-                    <button
-                        className="location-btn"
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            window.open(
-                                "https://www.google.com/maps/search/?api=1&query=Kurumboor+Mana+Nalukettu+Veedu+Guruvayur+Wedding+Police+Station+Police+Quarters+Rd+Chavakkad+Kerala+680506",
-                                "_blank"
-                            );
-                        }}
-                    >
-                        Location Map
-                    </button>
-                </div>
-
-                <div className="swipe-up">
-                    <span className="swipe-text">Swipe up</span>
-                    <span className="swipe-arrow">
-                        <FaLongArrowAltUp />
-                    </span>
-                </div>
-
+                        window.open(
+                            "https://www.google.com/maps/search/?api=1&query=Kurumboor+Mana+Nalukettu+Veedu+Guruvayur+Wedding+Police+Station+Police+Quarters+Rd+Chavakkad+Kerala+680506",
+                            "_blank"
+                        );
+                    }}
+                >
+                    Location Map
+                </button>
             </div>
-
+            <div className="swipe-up">
+                <span className="swipe-text">Swipe up</span>
+                <span className="swipe-arrow">
+                    <FaLongArrowAltUp />
+                </span>
+            </div>
         </div>
     );
 }
