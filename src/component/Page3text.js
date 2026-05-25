89495127d2d48150d992ad356835411a721a74f1
saptisunil1 <saptisunil1@gmail.com>
2026-05-25 11:57:07 +0100

    btn fix

--- src/component/Page3text.js
+++ src/component/Page3text.js
@@ -9,6 +9,7 @@ import { IoLocation } from "react-icons/io5"
 function Page3text({ onSwipeDown, onSwipeUp }) {
     const startY = useRef(0);
     const currentY = useRef(0);
+    const isTouchingButton = useRef(false);
 
     const handleTouchStart = (e) => {
         startY.current = e.touches[0].clientY;
@@ -19,7 +20,11 @@ function Page3text({ onSwipeDown, onSwipeUp }) {
     };
 
     const handleTouchEnd = () => {
+        if (isTouchingButton.current) return;
+
         const diff = startY.current - currentY.current;
+
+        if (Math.abs(diff) < 60) return;
 
         if (diff > 60) {
             onSwipeUp?.();
@@ -29,7 +34,7 @@ function Page3text({ onSwipeDown, onSwipeUp }) {
     };
 
     return (
-        <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} style={{ touchAction: "pan-y" }}>
+        <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
             <div className="text-overlay1">
                 <p className="p9">Ceremony</p>
                 <p>
@@ -41,12 +46,21 @@ function Page3text({ onSwipeDown, onSwipeUp }) {
                     <IoLocation size={60} color="white" />
                 </p>
                 <p className="p10">Kurumboor Mana</p>
-                <button className="location-btn" onClick={() =>
-                    window.open(
-                        "https://www.google.com/maps/search/?api=1&query=Kurumboor+Mana+Nalukettu+Veedu+Guruvayur+Wedding+Police+Station+Police+Quarters+Rd+Chavakkad+Kerala+680506",
-                        "_blank"
-                    )
-                }>Location Map</button>
+                <button
+                    className="location-btn"
+                    onTouchStart={() => (isTouchingButton.current = true)}
+                    onTouchEnd={() => (isTouchingButton.current = false)}
+                    onClick={(e) => {
+                        e.stopPropagation();
+
+                        window.open(
+                            "https://www.google.com/maps/search/?api=1&query=Kurumboor+Mana+Nalukettu+Veedu+Guruvayur+Wedding+Police+Station+Police+Quarters+Rd+Chavakkad+Kerala+680506",
+                            "_blank"
+                        );
+                    }}
+                >
+                    Location Map
+                </button>
             </div>
             <div className="swipe-up">
                 <span className="swipe-text">Swipe up</span>
