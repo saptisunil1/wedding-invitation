e8b54caccb8cb3dd739237fa71d48fb210582e48
saptisunil1 <saptisunil1@gmail.com>
2026-05-26 19:49:11 +0100

    documentation removed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-// import { useRef, useState } from 'react';
 import Start from './component/startPage';
-// import MusicIcon from './component/musicIcon';
 
 function App() {
-  // const audioRef = useRef(null);
-  // const [isPlaying, setIsPlaying] = useState(false);
-
-  // const toggleMusic = () => {
-  //   if (isPlaying) {
-  //     audioRef.current.pause();
-  //     setIsPlaying(false);
-  //   } else {
-  //     audioRef.current.play();
-  //     setIsPlaying(true);
-  //   }
-  // };
 
   return (
     <>
-      {/* <audio ref={audioRef} loop>
-        <source src="" type="audio/mp3" />
-      </audio>
-
-      <MusicIcon isPlaying={isPlaying} toggleMusic={toggleMusic} /> */}
       <Start></Start>
     </>
   );
